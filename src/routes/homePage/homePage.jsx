d01d118c1548c1adaca7ec5068a3af8b28c2b279
Aliyan Ahmed Khan <02-131222-070@student.bahria.edu.pk>
2024-11-30 15:15:44 +0500

    Till 41:40

---
+++ src/routes/homePage/homePage.jsx
@@ -0,0 +1,24 @@
+import './homePage.scss'
+
+function HomePage() {
+    return (
+        <div className='homePage'>
+            <div className="textContainer">
+                <div className="wrapper">
+                <h1 className='title'>
+                    Find Real Estate & Get Your Dream Place
+                    </h1>
+                    <p>
+                        Some description to write
+                    </p>
+                </div>
+            </div>
+            <div className="imgContainer">
+                <img src="/bg.png" alt="" />
+            </div>
+            </div>
+        
+    )
+}
+
+export default HomePage
