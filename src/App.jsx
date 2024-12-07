eb90e125bd40dc900e2555124e0a5b6662765344
Ismaaazam <ismaaazam090@gmail.com>
2024-12-07 17:36:13 +0500

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
-import Navbar from "./components/navbar/Navbar"
-import "./layout.scss"
 import HomePage from "./routes/homePage/homePage"
+import{
+  createBrowserRouter,
+  RouterProvider,
+  
+}from"react-router-dom";
+import ListPage from "./routes/homePage/listPage/listPage";
+import Layout from "./routes/homePage/layout/layout";
+import SinglePage from "./routes/homePage/singlePage/singlePage";
 
 function App() {
-  return (
-    <div className="layout">
-      <div className="navbar">
+  const router =createBrowserRouter([
+    {
+      path:"/",
+      element:<Layout/>,
+      children:[
+        {path:"/",
+         element:<HomePage/>
 
-      <Navbar/>
-      </div>     
-      <div className="content">
-      <HomePage/>
-      </div> 
-    </div>
-    
-  )
+        },
+        {path:"/list",
+          element:<ListPage/>
+ 
+        },
+        {path:"/:id",
+          element:<SinglePage/>
+ 
+        }
+      ]
+    }
+  ]);
+  return (
+   
+    <RouterProvider router={router}/>
+  );
 }
 
-export default App
+export default App;
